0271c2dfbd534b7efdda9e16ee54850a94da2075
MD RAZA KHAN <kmdraza47@gmail.com>
2025-08-17 19:43:26 +0530

    basic navbar

--- 10.Todo_App/src/components/Navbar.jsx
+++ 10.Todo_App/src/components/Navbar.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      hey im navbar
+    <nav className='flex justify-between bg-purple-600 text-white py-2'>
+  <div className="logo">
+     <span className="text-xl font-bold">Todo App</span>
     </div>
+   <ul className='flex space-x-4 mx-4'>
+    <li className='cursor-pointer hover:text-blue-500 font-bold'>home</li>
+      <li className='cursor-pointer hover:text-blue-500   font-bold'>Your Tasks</li>
+    
+   </ul>
+    </nav>
   )
 }
 
