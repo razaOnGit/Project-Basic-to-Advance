ca41daceb73981dc68d5d5f41461995463104b8a
MD RAZA KHAN <kmdraza47@gmail.com>
2025-08-24 19:23:38 +0530

    createApp

---
+++ 12.GetMeAChai/get-me-a-chai/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Get Me A Chai</h1>
+      <p>Your one-stop solution for all your chai needs!</p>
+    </div>
+  );
+}
